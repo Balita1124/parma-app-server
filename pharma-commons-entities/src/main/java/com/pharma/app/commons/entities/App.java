d8cd8f90e4b3dbc9cd71cc3b8fc314b28999bd7b
Rico Flavien FAUCHARD ANDRIANAIVO <randrianaivo@dsogroup.com>
2020-10-01 16:22:29 +0400

    Structuration

---
+++ pharma-commons-entities/src/main/java/com/pharma/app/commons/entities/App.java
@@ -0,0 +1,13 @@
+package com.pharma.app.commons.entities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
